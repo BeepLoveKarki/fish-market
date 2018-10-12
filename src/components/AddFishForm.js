03261c4807b8caa0b4870ca5e57b0cc043057d11
Biplab <biplabkarki04@gmail.com>
2018-10-12 20:29:54 +0545

    fishes

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class AddFishForm extends React.Component{
+
+    createFish(e){
+        e.preventDefault();
+        const fish={
+            name:this.name.value,
+            price:this.price.value,
+            status:this.status.value,
+            desc:this.desc.value,
+            url:this.url.value
+        };
+        this.props.addFish(fish);
+        this.fishform.reset();
+    }
+
+    render(){
+        return (
+            <form ref={(input)=>{this.fishform=input}} className="fish-edit" onSubmit={this.createFish.bind(this)}>
+               <input ref={(input)=>{this.name=input}} type="text" placeholder="Fish Name" required/>
+               <input ref={(input)=>{this.price=input}} type="number" placeholder="Fish Price" required/>
+               <select ref={(input)=>{this.status=input}} required>
+                   <option value="available">Fresh</option>
+                   <option value="unavailable">Sold Out</option>
+               </select>
+               <textarea ref={(input)=>{this.desc=input}} placeholder="Fish Description" required/>
+               <input type="url" ref={(input)=>{this.url=input}} placeholder="Image URL" required/>
+               <button type="submit">+ Add</button>
+            </form>
+        )
+    }
+}
+
+export default AddFishForm;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,26 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component{
+
+  gotoStore(e){
+    e.preventDefault();
+    let storeId=this.storeInput.value;
+    //this.context.router.transitionTo(`/store/${storeId}`);
+    this.props.history.push(`/store/${storeId}`);
+  }
+
   render(){
      return (
-       <form className="store-selector">
+       <form className="store-selector" onSubmit={this.gotoStore.bind(this)}>
          {/*Hello*/}
          <h2>Please enter a store</h2>
-         <input type="text" required placeholder="Enter store name" defaultValue={getFunName()}/>
+         <input type="text" required placeholder="Enter store name" 
+         defaultValue={getFunName()} ref={(input)=>{this.storeInput=input}}/>
          <button type="submit">Visit Store -></button>
        </form>
      )
   }
 }
+
 
 export default StorePicker;
